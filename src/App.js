ac095fd05d7b718b6d026b86973f29cc7ef648b7
Nik1-code <nik66998@gmail.com>
2021-03-25 13:44:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <ul>
+          <ul className="header-name">
+            <li>GuitarSrc</li>
+          </ul>
+          <ul className="header-language">
+            <li>SITE LANGUAGE:ENGLISH</li>
+          </ul>
+
+          <li>
+            <img className="arrow" src="down.png" />
+          </li>
+        </ul>
       </header>
+      <div>
+        <h3>Learn a Guitar for free.</h3>
+        <button>GET STARTED</button>
+        <button>I ALREADY HAVE AN ACCOUNT</button>
+      </div>
+      <div>
+        <h3>The best and easy way to learn an Instrument.</h3>
+        <p>
+          If you dont know how to play an instrument then no need to worry. We
+          are here to teach you from basic to advance. You are just one click
+          away from becoming the good player.
+        </p>
+      </div>
     </div>
   );
 }
